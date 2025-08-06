6c65566779f14c23835e7b8ed9fe6243e88c7bc3
test-one9 <Abdulghaniichbin121@outlook.com>
2025-08-06 09:19:51 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,119 +1,32 @@
-LOCAL_PATH := device/samsung/gtexswifi
+# Inherit from common
+$(call inherit-product, device/samsung/gtexs-common/gtexs-common.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/gtexswifi/overlay
 
-# Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/samsung/gtexswifi/gtexswifi-vendor.mk)
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/tablet-7in-hdpi-1024-hwui-memory.mk)
-
-# Boot animation
-TARGET_SCREEN_WIDTH := 800
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_BOOTANIMATION_HALF_RES := true
-
-# Audio
-PRODUCT_PACKAGES += \
-    audio_effects.conf \
-    audio_hw.xml \
-    audio_para \
-    audio_policy.conf \
-    codec_pga.xml \
-    tiny_hw.xml \
-    audio.primary.sc8830 \
-    audio_policy.sc8830 \
-    audio.r_submix.default \
-    audio.usb.default \
-    audio_vbc_eq \
-    libaudio-resampler \
-    libtinyalsa \
-    libeng-audio
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    bluetooth.default \
-    audio.a2dp.default \
-    libbt-vendor \
-    libbluetooth_jni
-
-# Camera
-PRODUCT_PACKAGES += \
-    camera.sc8830
-
-# Codecs
-PRODUCT_PACKAGES += \
-    libcolorformat_switcher \
-    libstagefrighthw \
-    libstagefright_sprd_aacdec \
-    libstagefright_sprd_mp3dec \
-    libstagefright_sprd_h264dec \
-    libstagefright_sprd_h264enc \
-    libstagefright_sprd_mpeg4dec \
-    libstagefright_sprd_mpeg4enc \
-    libstagefright_sprd_vpxdec
-
-# GPS
-PRODUCT_PACKAGES += \
-    gps.xml
-
-# Graphics & HWC
-PRODUCT_PACKAGES += \
-    libHWCUtils \
-    libGLES_mali.so \
-    memtrack.sc8830 \
-    gralloc.sc8830.so \
-    hwcomposer.sc8830 \
-    sprd_gsp.sc8830 \
-    libmemoryheapion \
-    libion_sprd \
-    libstagefright_shim \
-    libgps_shim
-
-# Keylayouts
+# Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/sci-keypad.kl:system/usr/keylayout/sci-keypad.kl \
-    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
+    device/samsung/gtexswifi/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl
 
-# Lights
-PRODUCT_PACKAGES += \
-    lights.sc8830
-
-# Media config
-MEDIA_XML_CONFIGS := \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml
+# Init scripts
 PRODUCT_COPY_FILES += \
-    $(foreach f,$(MEDIA_XML_CONFIGS),$(f):system/etc/$(notdir $(f)))
-PRODUCT_PACKAGES += \
-    media_codecs.xml \
-    media_profiles.xml
+    device/samsung/gtexswifi/rootdir/init.recovery.gtexswifi.rc:root/init.recovery.gtexswifi.rc \
+    device/samsung/gtexswifi/rootdir/init.gtexswifi.rc:root/init.gtexswifi.rc
 
-# PowerHAL
-PRODUCT_PACKAGES += \
-    power.sc8830
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
 
-# Ramdisk
-PRODUCT_PACKAGES += \
-    libinit_gtexswifi \
-    fstab.sc8830 \
-    fstab.unknown \
-    init.board.rc \
-    init.sc8830.rc \
-    init.sc8830.usb.rc \
-    init.storage.rc \
-    init.unknown.rc \
-    init.wifi.rc \
-    ueventd.sc8830.rc
+# Features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
 
-# WiFi
-PRODUCT_PACKAGES += \
-    p2p_supplicant_overlay.conf \
-    wpa_supplicant.conf \
-    wpa_supplicant_overlay.conf
+# Product characteristics
+PRODUCT_CHARACTERISTICS := tablet
+
+# Device identifier
+PRODUCT_NAME := gtexswifi
+PRODUCT_DEVICE := gtexswifi
+PRODUCT_MODEL := SM-T280
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
