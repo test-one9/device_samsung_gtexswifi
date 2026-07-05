f9482a6b2c6c219544f17bd76a3535c19271ea77
test-one9 <Abdulghaniichbin121@outlook.com>
2026-07-05 09:49:47 +0200

    Enhance camera configurations in BoardConfig.mk
    Added various camera configuration options including support for autofocus, face detection, and multiple capture modes.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,9 @@ TARGET_PREBUILT_KERNEL := device/samsung/gtexswifi/kernel/zImage
 TARGET_KERNEL_CONFIG := gtexswifi-dt_defconfig
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/gtexswifi/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8 androidboot.hardware=gtexswifi androidboot.selinux=permissive lpm_bootimg=vboot androidboot.bootdevice=soc.0/21900000.sdio
 
 # Partition Sizes (SM-T280 Stock partition map)
@@ -52,6 +55,126 @@ USE_OPENGL_RENDERER := true
 BOARD_USE_MALI_GRALLOC := true
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 BOARD_USES_LEGACY_MMAP := true
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+# Camera
+#zsl capture
+TARGET_BOARD_CAMERA_CAPTURE_MODE := false
+
+#back camera rotation capture
+TARGET_BOARD_BACK_CAMERA_ROTATION := false
+
+#front camera rotation capture
+TARGET_BOARD_FRONT_CAMERA_ROTATION := false
+
+#rotation capture
+TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
+
+# camera sensor type  #S5K4ECGA  #SR259
+CAMERA_SENSOR_TYPE_BACK := "s5k4ecgx_mipi"
+CAMERA_SENSOR_TYPE_FRONT := "sr352_mipi"
+
+# select camera 2M,3M,5M,8M
+CAMERA_SUPPORT_SIZE := 5M
+FRONT_CAMERA_SUPPORT_SIZE := 2M
+
+TARGET_BOARD_NO_FRONT_SENSOR := false
+TARGET_BOARD_CAMERA_FLASH_CTRL := false
+
+#read otp method 1:from kernel 0:from user
+TARGET_BOARD_CAMERA_READOTP_METHOD := 1
+
+#face detect
+TARGET_BOARD_CAMERA_FACE_DETECT := false
+
+#sensor interface
+TARGET_BOARD_BACK_CAMERA_INTERFACE := mipi
+TARGET_BOARD_FRONT_CAMERA_INTERFACE := mipi
+
+#select camera zsl cap mode
+TARGET_BOARD_CAMERA_CAPTURE_MODE := true
+
+#select camera zsl force cap mode
+TARGET_BOARD_CAMERA_FORCE_ZSL_MODE := true
+
+#sprd zsl feature
+TARGET_BOARD_CAMERA_SPRD_PRIVATE_ZSL := true
+
+#rotation capture
+TARGET_BOARD_CAMERA_ROTATION_CAPTURE := false
+
+#select camera support autofocus
+TARGET_BOARD_CAMERA_AUTOFOCUS := true
+
+TARGET_BOARD_CAMERA_FACE_BEAUTY := false
+
+#uv denoise enable
+TARGET_BOARD_CAMERA_CAPTURE_DENOISE := false
+
+#y denoise enable
+TARGET_BOARD_CAMERA_Y_DENOISE := true
+
+#select continuous auto focus
+TARGET_BOARD_CAMERA_CAF := true
+
+TARGET_BOARD_CAMERA_NO_FLASH_DEV := true
+
+#image angle in different project
+TARGET_BOARD_CAMERA_ADAPTER_IMAGE := 0
+
+#pre_allocate capture memory
+TARGET_BOARD_CAMERA_PRE_ALLOC_CAPTURE_MEM := true
+
+#sc8830g isp ver 0;sc9630 isp ver 1;sp9832a_2h11 isp version 2
+TARGET_BOARD_CAMERA_ISP_SOFTWARE_VERSION := 2
+
+TARGET_BOARD_CAMERA_ISP_AE_VERSION := 0
+
+#set hal version to 1.0
+TARGET_USES_MEDIA_EXTENSIONS := true
+TARGET_BOARD_CAMERA_HAL_VERSION := 1.0
+
+TARGET_BOARD_USE_THRID_LIB := true
+TARGET_BOARD_USE_THIRD_AWB_LIB_A := true
+TARGET_BOARD_USE_ALC_AE_AWB := false
+TARGET_BOARD_USE_THIRD_AF_LIB_A := true
+
+TARGET_VCM_BU64241GWZ := true
+
+#support auto anti-flicker
+TARGET_BOARD_CAMERA_ANTI_FLICKER := true
+
+#multi cap memory mode
+TARGET_BOARD_MULTI_CAP_MEM := true
+
+#low capture memory
+TARGET_BOARD_LOW_CAPTURE_MEM := true
+
+#select mipi d-phy mode(none, phya, phyb, phyab)
+TARGET_BOARD_FRONT_CAMERA_MIPI := phyb
+TARGET_BOARD_BACK_CAMERA_MIPI := phya
+
+#select ccir pclk src(source0, source1)
+TARGET_BOARD_FRONT_CAMERA_CCIR_PCLK := source0
+TARGET_BOARD_BACK_CAMERA_CCIR_PCLK := source0
+
+# misc
+TARGET_HAS_BACKLIT_KEYS := false
+
+# System properties
+TARGET_SYSTEM_PROP += device/samsung/gtexswifi/system.prop
+
+# Use dmalloc() for such low memory devices like us
+MALLOC_SVELTE := true
+BOARD_USES_LEGACY_MMAP := true
+
+# Bionic
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+
+
 
 # Connectivity (Broadcom/Spreadtrum integration)
 BOARD_HAVE_BLUETOOTH := true
@@ -61,9 +184,16 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_HOSTAPD_DRIVER := NL80211
 
+TARGET_KERNEL_MODULES := SPRDWL_MODULE
+
+SPRDWL_MODULE:
+	mv $(KERNEL_OUT)/drivers/net/wireless/sc2331/sprdwl.ko $(KERNEL_MODULES_OUT)
+
+TARGET_RECOVERY_FSTAB = device/samsung/gtexswifi/rootdir/recovery.fstab
+LZMA_RAMDISK_TARGETS := recovery
 # SELinux State
-BOARD_SEPOLICY_DIRS += device/samsung/gtexswifi/sepolicy
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_SEPOLICY_DIRS += device/samsung/gtexswifi/sepolicy
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Treble Compatibility Configuration
 PRODUCT_FULL_TREBLE_OVERRIDE := false
